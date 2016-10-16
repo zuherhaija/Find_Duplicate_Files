a79627b25bcc42c497492c6f8d7fd068d5618738
shoujiaxin <shoujiaxin@gmail.com>
2016-10-16 17:58:20 +0800

    Find duplicate files
    A small tool written in C# to find duplicate files on your PC

---
+++ FindDuplicateFiles/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FindDuplicateFiles
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lvwResult = new System.Windows.Forms.ListView();
+            this.lblProcess = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(511, 11);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnFolder.TabIndex = 0;
+            this.btnFolder.Text = "浏览";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            this.btnFolder.Click += new System.EventHandler(this.btnFolder_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(81, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(400, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 523);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "已查找文件数";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(93, 523);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(11, 12);
+            this.lblCounter.TabIndex = 3;
+            this.lblCounter.Text = "0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(604, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lvwResult
+            // 
+            this.lvwResult.FullRowSelect = true;
+            this.lvwResult.GridLines = true;
+            this.lvwResult.Location = new System.Drawing.Point(12, 40);
+            this.lvwResult.Name = "lvwResult";
+            this.lvwResult.Size = new System.Drawing.Size(760, 480);
+            this.lvwResult.TabIndex = 5;
+            this.lvwResult.UseCompatibleStateImageBehavior = false;
+            this.lvwResult.View = System.Windows.Forms.View.Details;
+            // 
+            // lblProcess
+            // 
+            this.lblProcess.AutoSize = true;
+            this.lblProcess.Location = new System.Drawing.Point(10, 540);
+            this.lblProcess.Name = "lblProcess";
+            this.lblProcess.Size = new System.Drawing.Size(0, 12);
+            this.lblProcess.TabIndex = 6;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(697, 11);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 7;
+            this.btnAbout.Text = "关于";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "目标文件夹";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.lblProcess);
+            this.Controls.Add(this.lvwResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "文件查重";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolder;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListView lvwResult;
+        private System.Windows.Forms.Label lblProcess;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FindDuplicateFiles/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindDuplicateFiles
+{
+    public partial class frmMain : Form
+    {
+        public static string folderPath;
+        public static int counter = 0;
+        public static int result = 0;
+        public static List<string> fileInfo = new List<string>();
+        public static List<string> fileName = new List<string>();
+        public static List<string> filePath = new List<string>();
+        public static List<long> fileSize = new List<long>();
+        public static bool finding = false;
+        public static bool stoping = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            // lvwResult初始化
+            this.lvwResult.Columns.Add("文件名", 200, System.Windows.Forms.HorizontalAlignment.Left);
+            this.lvwResult.Columns.Add("位置", 460, System.Windows.Forms.HorizontalAlignment.Left);
+            this.lvwResult.Columns.Add("大小", 100, System.Windows.Forms.HorizontalAlignment.Left);
+        }
+
+        // 选择目标文件夹
+        private void btnFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+
+            // 隐藏“新建文件夹”按钮
+            folderDlg.ShowNewFolderButton = false;
+            folderDlg.Description = "选择需要查重的文件夹";
+
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                // 获取路径
+                folderPath = folderDlg.SelectedPath;
+                // 显示路径
+                txtPath.Text = folderPath;
+            }
+            // 释放
+            folderDlg.Dispose();
+        }
+
+        // 遍历文件夹
+        private void ListFiles(FileSystemInfo info)
+        {
+            // 中止
+            if (stoping)
+            {
+                return;
+            }
+
+            // 路径错误
+            if (!info.Exists)
+            {
+                MessageBox.Show("文件不存在！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DirectoryInfo dir = info as DirectoryInfo;
+
+            // 不是目录
+            if (dir == null)
+            {
+                return;
+            }
+
+            FileSystemInfo[] files = dir.GetFileSystemInfos();
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = files[i] as FileInfo;
+
+                // 文件
+                if (file != null)
+                {
+                    // 计算MD5数值
+                    FileStream fileStream = file.OpenRead();
+                    MD5CryptoServiceProvider fileMD5 = new MD5CryptoServiceProvider();
+                    byte[] hashByte = fileMD5.ComputeHash(fileStream);
+                    string str = System.BitConverter.ToString(hashByte);
+                    str = str.Replace("-", "");
+                    //str = str.ToLower();
+
+                    // 释放流
+                    fileStream.Close();
+                    
+                    // 记录
+                    counter++;
+                    fileInfo.Add(str);
+                    fileName.Add(file.Name);
+                    filePath.Add(file.DirectoryName);
+                    fileSize.Add(file.Length);
+
+                    // 显示
+                    lblCounter.Text = Convert.ToString(counter);
+                    lblProcess.Text = "正在查找：" + file.DirectoryName + "\\" + file.Name;
+                    lblCounter.Update();
+                    lblProcess.Update();
+                }
+                // 文件夹
+                else
+                {
+                    ListFiles(files[i]);
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // 初始化
+            counter = 0;
+            result = 0;
+            fileInfo.Clear();
+            fileName.Clear();
+            filePath.Clear();
+            fileSize.Clear();
+            lvwResult.Clear();
+            this.lvwResult.Columns.Add("文件名", 200, System.Windows.Forms.HorizontalAlignment.Left);
+            this.lvwResult.Columns.Add("位置", 460, System.Windows.Forms.HorizontalAlignment.Left);
+            this.lvwResult.Columns.Add("大小", 100, System.Windows.Forms.HorizontalAlignment.Left);
+
+            if (folderPath != null)
+            {
+                finding = true;
+                stoping = false;
+                btnStart.Text = "中止";
+                btnStart.Update();
+
+                ListFiles(new DirectoryInfo(folderPath));
+
+                // 判断
+                int f = fileName.Count();
+
+                for (int i = 0; i < f; i++)
+                {
+                    for (int j = i + 1; j < f; j++)
+                    {
+                        if (fileInfo[i] == fileInfo[j] && fileSize[i] == fileSize[j])
+                        {
+                            result++;
+
+                            // 显示结果
+                            ListViewItem item1 = new ListViewItem();
+                            item1.SubItems.Clear();
+                            item1.SubItems[0].Text = fileName[i];
+                            item1.SubItems.Add(filePath[i]);
+                            item1.SubItems.Add(fileSize[i].ToString());
+
+                            ListViewItem item2 = new ListViewItem();
+                            item2.SubItems.Clear();
+                            item2.SubItems[0].Text = fileName[j];
+                            item2.SubItems.Add(filePath[j]);
+                            item2.SubItems.Add(fileSize[j].ToString());
+
+                            lvwResult.Items.Add(item1);
+                            lvwResult.Items.Add(item2);
+                        }
+                    }
+                }
+
+                lblProcess.Text = "查找完毕，已找到" + result.ToString() + "个重复文件";
+
+                if (result == 0)
+                {
+                    MessageBox.Show("未找到重复文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+
+                // 复位
+                finding = false;
+                btnStart.Text = "开始";
+            }
+            else
+            {
+                MessageBox.Show("请选择目标文件夹", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.Show();
+        }
+    }
+}
